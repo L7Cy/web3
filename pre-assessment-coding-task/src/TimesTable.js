23a9ac19996bbd130c5360a641c30b8f0282a6aa
L7Cy <l7cy.at@gmail.com>
2024-04-10 19:50:15 +0900

    クリックしたマスの縦と横をハイライトする機能を追加

--- pre-assessment-coding-task/src/TimesTable.js
+++ pre-assessment-coding-task/src/TimesTable.js
@@ -3,6 +3,10 @@ import React, { useState, useEffect } from "react";
 const TimesTable = () => {
   const [data, setData] = useState([]);
   const [highlightedNumber, setHighlightedNumber] = useState(null);
+  const [highlightedCoords, setHighlightedCoords] = useState({
+    row: null,
+    col: null,
+  });
 
   useEffect(() => {
     const fetchTimesTable = async () => {
@@ -14,29 +18,57 @@ const TimesTable = () => {
     fetchTimesTable();
   }, []);
 
+  const handleClick = (rowIndex, cellIndex, cellValue) => {
+    setHighlightedCoords({ row: rowIndex, col: cellIndex });
+    setHighlightedNumber(cellValue);
+  };
+
   return (
     <table>
       <thead>
         <tr>
-          <th></th> {}
+          <th></th>
           {Array.from({ length: 12 }, (_, i) => (
-            <th key={i}>{i + 1}</th>
+            <th
+              key={i}
+              className={highlightedCoords.col === i ? "row-col-highlight" : ""}
+            >
+              {i + 1}
+            </th>
           ))}
         </tr>
       </thead>
       <tbody>
         {data.map((row, rowIndex) => (
           <tr key={rowIndex}>
-            <th>{rowIndex + 1}</th> {}
-            {row.map((cell, cellIndex) => (
-              <td
-                key={cellIndex}
-                className={cell === highlightedNumber ? "highlight" : ""}
-                onClick={() => setHighlightedNumber(cell)}
-              >
-                {cell}
-              </td>
-            ))}
+            <th
+              className={
+                highlightedCoords.row === rowIndex ? "row-col-highlight" : ""
+              }
+            >
+              {rowIndex + 1}
+            </th>
+            {row.map((cell, cellIndex) => {
+              const isHighlightedNumber = cell === highlightedNumber;
+              const isRowOrColHighlight =
+                highlightedCoords.row === rowIndex ||
+                highlightedCoords.col === cellIndex;
+              const className = isHighlightedNumber
+                ? "highlight"
+                : isRowOrColHighlight
+                ? "row-col-highlight"
+                : "";
+
+              return (
+                <td
+                  key={cellIndex}
+                  className={className}
+                  onClick={() => handleClick(rowIndex, cellIndex, cell)}
+                >
+                  {cell}
+                </td>
+              );
+            })}
           </tr>
         ))}
       </tbody>
