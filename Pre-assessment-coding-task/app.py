1be7a3bbeaffd7e63b1c9ccd03154ac4771ffe50
L7Cy <l7cy.at@gmail.com>
2024-04-10 16:34:48 +0900

    variant6まで

--- Pre-assessment-coding-task/app.py
+++ Pre-assessment-coding-task/app.py
@@ -1,11 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 
-@app.route("/")
-def index():
-    return render_template("index.html")
+@app.route("/times-table", methods=["GET"])
+def times_table():
+    table = [[i * j for j in range(1, 13)] for i in range(1, 13)]
+    return jsonify(table)
 
 
 if __name__ == "__main__":
--- Pre-assessment-coding-task/timesTable.py
+++
@@ -1,7 +0,0 @@
-class TimesTable:
-    @classmethod
-    def print_table(cls):
-        for row in range(1, 13):
-            for col in range(1, 13):
-                print(f"{row * col:4}", end="")
-            print()
